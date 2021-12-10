eb1f7864c61ee80d0d25c4837b22a5e5a63c4559
samukes <darkdarumia@gmail.com>
2021-12-09 22:14:33 -0300

    Handling User creation

--- lib/exlivery.ex
+++ lib/exlivery.ex
@@ -1,2 +1,10 @@
 defmodule Exlivery do
+  alias Exlivery.Users.CreateOrUpdate
+  alias Exlivery.Users.Agent, as: UserAgent
+
+  def start_agents do
+    UserAgent.start_link(%{})
+  end
+
+  defdelegate create_or_update_user(params), to: CreateOrUpdate, as: :call
 end
