0c92f26ec140aeca038d068597aacbda9c38e2d6
samukes <darkdarumia@gmail.com>
2021-12-09 23:11:37 -0300

    Adding orders agent

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule Exlivery.MixProject do
     [
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:decimal, "~> 2.0"},
-      {:ex_machina, "~> 2.7.0", only: :test}
+      {:ex_machina, "~> 2.7.0", only: :test},
+      {:uuid, "~> 1.1"}
     ]
   end
 
